99a9c0c10d1ba1dbfcfd244d7873dbaed96dab00
OSSINOVA <oscar.dyremyhr@gmail.com>
2015-09-16 20:22:56 -0400

    Merge origin/master
    Conflicts:
    	Application/src/application/Application.java

---
+++ Application/src/application/mainFrame.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+/**
+ *
+ * @author ocd5012
+ */
+public class mainFrame {
+    public static void main(String [] args)
+	{
+		//test
+	}
+
+}
+
