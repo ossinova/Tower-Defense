d4915bd676069069bb91cabcfb88694f24d4aa72
asl5257 <asl5257@206-L51.up.ist.local>
2015-10-14 19:29:32 -0400

    Turrets added to TurretListView

--- Application/src/application/TurretListView.java
+++ Application/src/application/TurretListView.java
@@ -18,10 +18,38 @@ public class TurretListView
     public TurretListView()
     {
         activeTurretList = new ArrayList<Turret>();
+        turretSetup();
     }
     
     public ArrayList<Turret> getActiveTurretList()
     {
         return activeTurretList;
+    }
+    
+    public void turretSetup()
+    {
+        Turret paper = new Turret();
+          paper.setCost(10);
+          paper.setDamage(5);
+          paper.setRange(7);
+          paper.setRateOfFire(3);
+          paper.setIsLocked(false);
+        activeTurretList.add(paper);
+        
+        Turret eraser = new Turret();
+          eraser.setCost(50);
+          eraser.setDamage(20);
+          eraser.setRange(3);
+          eraser.setRateOfFire(1);
+          eraser.setIsLocked(false);
+        activeTurretList.add(eraser);
+        
+        Turret pencil = new Turret();
+          pencil.setCost(25);
+          pencil.setDamage(10);
+          pencil.setRange(5);
+          pencil.setRateOfFire(2);
+          pencil.setIsLocked(false);
+        activeTurretList.add(pencil);
     }
 }
